3d97ce2000733925488308d4d5d4bc2240a5e96f
Zhiyuanchan <2005223i@student.tp.edu.sg>
2022-02-23 02:02:51 +0800

    Test done

---
+++ src/test/java/NotePad2/NoteTest.java
@@ -0,0 +1,49 @@
+package NotePad2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class NoteTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetUser() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testSetUser() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testGetTitle() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testSetTitle() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testGetDetails() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testSetDetails() {
+		assertEquals(true, true);
+	}
+
+}
---
+++ src/test/java/NotePad2/ServletTest.java
@@ -0,0 +1,54 @@
+package NotePad2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ServletTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetConnection() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse1() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse1() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testDoPut() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	void testDoDelete() {
+		assertEquals(true, true);
+	}
+
+}
