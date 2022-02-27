5e371ce42519351aa61f0f447f5eb9ff4cffb30c
Zhiyuanchan <2005223i@student.tp.edu.sg>
2022-02-27 17:02:59 +0800

    update

--- src/test/java/NotePad2/NewTest.java
+++ src/test/java/NotePad2/NewTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
@@ -18,8 +19,18 @@ public class NewTest {
 		  // nagivate the browser to this url
 		  driver.get("http://localhost:8083/NotePad2/NoteServlet/dashboard");
 		  // browser look for link with text value "Add New User"
-		  driver.findElement(By.linkText("Add New User")).click();
+		  //driver.findElement(By.linkText("Add New User")).click();
+		  //WebElement add = driver.findElement(By.name("Add New User"));
+		  //add.click();
+		  //WebElement name = driver.findElement(By.name("Name"));
+		  //WebElement title = driver.findElement(By.name("Title"));
+		  //WebElement details = driver.findElement(By.name("Details"));
+		  //WebElement submit = driver.findElement(By.name("Call Servlet"));
 		  
+		  //name.sendKeys("sel");
+		  //title.sendKeys("sel");
+		  //details.sendKeys("sel");
+		  //submit.click();
 		  //driver.findElement(By.id("Name")).sendKeys("h");
 		  //driver.findElement(By.id("Title")).sendKeys("h");
 		  //driver.findElement(By.id("Details")).sendKeys("h");
@@ -27,7 +38,7 @@ public class NewTest {
 		  //driver.findElement(By.linkText("Call Servlet")).click();
 		  
 		  // browser look for link with text value "Edit"
-		  //driver.findElement(By.linkText("Edit")).click();
+		  driver.findElement(By.linkText("Edit")).click();
   }
   @BeforeTest
   public void beforeTest() {
